1e957b700da1a34788154c67b070ee43ebcfb0c3
Andrei Paduraru <83088926+Padure3@users.noreply.github.com>
2022-07-06 18:07:59 +0100

    updated to populate price and link columns

--- stockchecker.py
+++ stockchecker.py
@@ -13,13 +13,16 @@ API_KEYS_FILE = 'C:\stockchecker\mouser_api_keys.yaml'
 
 #Excel
 stocksheet = load_workbook(filename="stock.xlsx")
-sheet = stocksheet.active
+sheet = stocksheet.worksheets[0]
 datalen = sheet.max_row-1
+
+stend = ' In Stock'
 
 stock = []
 for i in range(0,datalen):
     args = []
     partno = str(sheet['A'+str(2+i)].value) #excel
+    qneeded = int(sheet['D'+str(2+i)].value)
     #print(read.iloc[i][0]) #google sheets
     print(partno)
     request = MouserPartSearchRequest('partnumber', API_KEYS_FILE, *args)
@@ -28,9 +31,24 @@ for i in range(0,datalen):
             search = request.part_search(partno)
             if search:
                 # Print result
-                respraw = request.get_clean_response()
-                stock.append([respraw["Availability"]])
-                sheet["F"+str(2+i)] = respraw["Availability"] #excel
+                try:
+                    respraw = request.get_clean_response()
+                    for j in range (0,len(respraw["PriceBreaks"])-1):
+                        if j < len(respraw["PriceBreaks"])-1:
+                            qlow = int(respraw["PriceBreaks"][j]["Quantity"])
+                            qhigh = int((respraw["PriceBreaks"][j+1]["Quantity"]))
+                            if qneeded >= qlow and qneeded < qhigh:
+                                price = float(respraw["PriceBreaks"][j]["Price"].removeprefix('£'))
+                        else:
+                            price = float(respraw["PriceBreaks"][j]["Price"].removeprefix('£'))
+                            
+                    #stock.append([respraw["Availability"]])
+                    sheet["B"+str(2+i)] = respraw["ProductDetailUrl"]
+                    sheet["C"+str(2+i)] = price
+                    sheet["F"+str(2+i)] = int(respraw["Availability"].removesuffix(stend)) #excel
+                    
+                except Exception:
+                    pass
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S on %d/%m/%Y")
